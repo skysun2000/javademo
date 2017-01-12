55d283ffcc34cbcfed84fa9fe41c88bd1d99d09b
Tian-Yang Chen <skysunchen@live.com>
2017-01-12 14:00:23 +0800

    add 2nd pom file

--- src/test/java/payslip/Uat.java
+++ src/test/java/payslip/Uat.java
@@ -1,33 +1,33 @@
-package payslip;
-
-
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.Assert;
-//import org.testng.annotations.AfterTest;
-//import org.testng.annotations.BeforeTest;
-//import org.testng.annotations.Test;
-
-
-public class Uat {
-//    private WebDriver driver;
-    @Test
-    public void testEasy() {
-//        driver.get("http://jenserver2016.centralus.cloudapp.azure.com:8080/payslip-0.0.1/");
-//        String title = driver.getTitle();
-//        Assert.assertTrue(title.contains("PAY SLIP"));
-        Assert.assertTrue(true);
-    }
-//    @BeforeTest
-//    public void beforeTest() {
-////        driver = new FirefoxDriver();
-//    }
-//    @AfterTest
-//    public void afterTest() {
+//package payslip;
 //
-////        driver.quit();
+//
+//
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+////import org.openqa.selenium.WebDriver;
+////import org.openqa.selenium.firefox.FirefoxDriver;
+////import org.testng.Assert;
+////import org.testng.annotations.AfterTest;
+////import org.testng.annotations.BeforeTest;
+////import org.testng.annotations.Test;
+//
+//
+//public class Uat {
+////    private WebDriver driver;
+//    @Test
+//    public void testEasy() {
+////        driver.get("http://jenserver2016.centralus.cloudapp.azure.com:8080/payslip-0.0.1/");
+////        String title = driver.getTitle();
+////        Assert.assertTrue(title.contains("PAY SLIP"));
+//        Assert.assertTrue(true);
 //    }
-}   
+////    @BeforeTest
+////    public void beforeTest() {
+//////        driver = new FirefoxDriver();
+////    }
+////    @AfterTest
+////    public void afterTest() {
+////
+//////        driver.quit();
+////    }
+//}
