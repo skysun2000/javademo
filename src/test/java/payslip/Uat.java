fd0f3c8f58878585e38ddea5b7219dec9a12351a
Tian-Yang Chen <skysunchen@live.com>
2017-01-12 14:06:57 +0800

    add testng test case.

--- src/test/java/payslip/Uat.java
+++ src/test/java/payslip/Uat.java
@@ -2,32 +2,32 @@
 //
 //
 //
+import org.testng.Assert;
+import org.testng.annotations.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.testng.Assert;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeTest;
 //import org.testng.annotations.Test;
-////import org.openqa.selenium.WebDriver;
-////import org.openqa.selenium.firefox.FirefoxDriver;
-////import org.testng.Assert;
-////import org.testng.annotations.AfterTest;
-////import org.testng.annotations.BeforeTest;
-////import org.testng.annotations.Test;
-//
-//
-//public class Uat {
-////    private WebDriver driver;
-//    @Test
-//    public void testEasy() {
-////        driver.get("http://jenserver2016.centralus.cloudapp.azure.com:8080/payslip-0.0.1/");
-////        String title = driver.getTitle();
-////        Assert.assertTrue(title.contains("PAY SLIP"));
-//        Assert.assertTrue(true);
+
+
+public class Uat {
+//    private WebDriver driver;
+    @Test
+    public void testEasy() {
+//        driver.get("http://jenserver2016.centralus.cloudapp.azure.com:8080/payslip-0.0.1/");
+//        String title = driver.getTitle();
+//        Assert.assertTrue(title.contains("PAY SLIP"));
+        Assert.assertTrue(true);
+    }
+//    @BeforeTest
+//    public void beforeTest() {
+////        driver = new FirefoxDriver();
 //    }
-////    @BeforeTest
-////    public void beforeTest() {
-//////        driver = new FirefoxDriver();
-////    }
-////    @AfterTest
-////    public void afterTest() {
-////
-//////        driver.quit();
-////    }
-//}
+//    @AfterTest
+//    public void afterTest() {
+//
+////        driver.quit();
+//    }
+}
